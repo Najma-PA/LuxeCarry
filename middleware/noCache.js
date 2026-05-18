4895690378efe17e5cfb54298fdbd367b7ae5b0a
Najma PA <najma.pabbas@gmail.com>
2026-05-18 17:21:58 +0400

    Refactor project structure

--- middleware/noCache.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0, private');
-  res.setHeader('Pragma', 'no-cache');
-  res.setHeader('Expires', '-1');
-  next();
-};
