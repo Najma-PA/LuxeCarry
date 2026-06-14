337fd32d5a1d8020227bd57c21b0a1befb850c2a
Najma PA <najma.pabbas@gmail.com>
2026-06-14 17:15:18 +0400

    passport.js updated

--- src/config/passport.js
+++ src/config/passport.js
@@ -9,7 +9,6 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      //callbackURL: 'http://localhost:3000/user/auth/google/callback',
       callbackURL: 'https://luxecarry.na-jma.xyz/user/auth/google/callback',
     },
     async (accessToken, refreshToken, profile, done) => {
